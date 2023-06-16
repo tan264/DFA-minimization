5f48269d07f6484340425a76af72022e828a5e4d
tan264 <tandang264@gmail.com>
2023-06-16 11:33:48 +0700

    Remove redundant code

--- algo/MyhillNerode.java
+++ algo/MyhillNerode.java
@@ -5,8 +5,6 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-import javax.sound.midi.Track;
-
 import model.Automaton;
 
 enum Status {
@@ -43,6 +41,100 @@ public class MyhillNerode implements Algorithm {
 		}
 
 		// Step 3
+		return buildNewAutomaton(dfa);
+	}
+
+	private void cloneDFA(Automaton dfa) {
+		Set<String> states = new HashSet<>();
+		states.addAll(dfa.getStates());
+
+		Set<String> symbols = new HashSet<>();
+		symbols.addAll(dfa.getSymbols());
+
+		Set<String> finalStates = new HashSet<>();
+		finalStates.addAll(dfa.getFinalStates());
+
+		Map<String, Map<String, String>> transitions = new HashMap<>();
+		transitions.putAll(dfa.getTransitions());
+
+		dfa = new Automaton(states, symbols, dfa.getStartState(), finalStates, transitions);
+	}
+
+	private void deleteUnreachableState(Automaton dfa) {
+		Set<String> reachableStates = new HashSet<>();
+		for (String state : dfa.getStates()) {
+			for (String inputSymbol : dfa.getSymbols()) {
+				reachableStates.add(dfa.getTransitions().get(state).get(inputSymbol));
+			}
+		}
+
+		Set<String> originalStates = new HashSet<>();
+		originalStates.addAll(dfa.getStates());
+
+		for (String state : originalStates) {
+			if (!state.equals(dfa.getStartState()) && !reachableStates.contains(state)) {
+				dfa.getStates().remove(state);
+				dfa.getTransitions().remove(state);
+				dfa.getFinalStates().remove(state);
+			}
+		}
+	}
+
+	private void initTable(Automaton dfa) {
+		Set<String> vistedStates = new HashSet<>();
+		for (String state1 : dfa.getStates()) {
+			vistedStates.add(state1);
+			Map<String, Status> status = new HashMap<>();
+			for (String state2 : dfa.getStates()) {
+				if (!vistedStates.contains(state2)) {
+					if ((dfa.isFinalState(state1) && !dfa.isFinalState(state2))
+							|| (!dfa.isFinalState(state1) && dfa.isFinalState(state2))) {
+						status.put(state2, Status.MARKED);
+					} else {
+						status.put(state2, Status.UNMARKED);
+					}
+
+				}
+			}
+			if (status.size() > 0) {
+				table.put(state1, status);
+			}
+		}
+	}
+
+	private boolean canMark(String state1, String state2, Automaton dfa) {
+		Map<String, Map<String, String>> transitions = dfa.getTransitions();
+		for (String inputSymbol : dfa.getSymbols()) {
+			String temp1 = transitions.get(state1).get(inputSymbol);
+			String temp2 = transitions.get(state2).get(inputSymbol);
+
+			if (table.containsKey(temp1) && table.containsKey(temp2)) {
+				if (table.get(temp1).get(temp2) == Status.MARKED || table.get(temp2).get(temp1) == Status.MARKED) {
+					return true;
+				}
+			} else if (table.containsKey(temp1)) {
+				if (table.get(temp1).get(temp2) == Status.MARKED) {
+					return true;
+				}
+			} else if (table.containsKey(temp2)) {
+				if (table.get(temp2).get(temp1) == Status.MARKED) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	private void mark(String state1, String state2) {
+		if (table.containsKey(state1)) {
+			table.get(state1).replace(state2, Status.MARKED);
+		} else if (table.containsKey(state2)) {
+			table.get(state2).replace(state1, Status.MARKED);
+		}
+	}
+
+	private Automaton buildNewAutomaton(Automaton dfa) {
 		StringBuilder sb = new StringBuilder();
 		Set<String> states = new HashSet<>();
 		String startState = dfa.getStartState();
@@ -90,8 +182,9 @@ public class MyhillNerode implements Algorithm {
 //					if (state2.contains(transitions.get(state1).get(symbol))) {
 //						transitions.get(state1).replace(symbol, state2);
 //					}
-					
-					if(transitions.get(state1).containsKey(symbol) && state2.contains(transitions.get(state1).get(symbol))) {
+
+					if (transitions.get(state1).containsKey(symbol)
+							&& state2.contains(transitions.get(state1).get(symbol))) {
 						transitions.get(state1).replace(symbol, state2);
 					}
 				}
@@ -100,95 +193,4 @@ public class MyhillNerode implements Algorithm {
 
 		return new Automaton(states, dfa.getSymbols(), startState, finalStates, transitions);
 	}
-
-	private void cloneDFA(Automaton dfa) {
-		Set<String> states = new HashSet<>();
-		states.addAll(dfa.getStates());
-
-		Set<String> symbols = new HashSet<>();
-		symbols.addAll(dfa.getSymbols());
-
-		Set<String> finalStates = new HashSet<>();
-		finalStates.addAll(dfa.getFinalStates());
-
-		Map<String, Map<String, String>> transitions = new HashMap<>();
-		transitions.putAll(dfa.getTransitions());
-
-		dfa = new Automaton(states, symbols, dfa.getStartState(), finalStates, transitions);
-	}
-
-	private void initTable(Automaton dfa) {
-		Set<String> vistedStates = new HashSet<>();
-		for (String state1 : dfa.getStates()) {
-			vistedStates.add(state1);
-			Map<String, Status> status = new HashMap<>();
-			for (String state2 : dfa.getStates()) {
-				if (!vistedStates.contains(state2)) {
-					if ((dfa.isFinalState(state1) && !dfa.isFinalState(state2))
-							|| (!dfa.isFinalState(state1) && dfa.isFinalState(state2))) {
-						status.put(state2, Status.MARKED);
-					} else {
-						status.put(state2, Status.UNMARKED);
-					}
-
-				}
-			}
-			if (status.size() > 0) {
-				table.put(state1, status);
-			}
-		}
-	}
-
-	private void deleteUnreachableState(Automaton dfa) {
-		Set<String> reachableStates = new HashSet<>();
-		for (String state : dfa.getStates()) {
-			for (String inputSymbol : dfa.getSymbols()) {
-				reachableStates.add(dfa.getTransitions().get(state).get(inputSymbol));
-			}
-		}
-
-		Set<String> originalStates = new HashSet<>();
-		originalStates.addAll(dfa.getStates());
-
-		for (String state : originalStates) {
-			if (!state.equals(dfa.getStartState()) && !reachableStates.contains(state)) {
-				dfa.getStates().remove(state);
-				dfa.getTransitions().remove(state);
-				dfa.getFinalStates().remove(state);
-			}
-		}
-	}
-
-	private boolean canMark(String state1, String state2, Automaton dfa) {
-		Map<String, Map<String, String>> transitions = dfa.getTransitions();
-		for (String inputSymbol : dfa.getSymbols()) {
-			String temp1 = transitions.get(state1).get(inputSymbol);
-			String temp2 = transitions.get(state2).get(inputSymbol);
-
-			if (table.containsKey(temp1) && table.containsKey(temp2)) {
-				if (table.get(temp1).get(temp2) == Status.MARKED || table.get(temp2).get(temp1) == Status.MARKED) {
-					return true;
-				}
-			} else if (table.containsKey(temp1)) {
-				if (table.get(temp1).get(temp2) == Status.MARKED) {
-					return true;
-				}
-			} else if (table.containsKey(temp2)) {
-				if (table.get(temp2).get(temp1) == Status.MARKED) {
-					return true;
-				}
-			}
-		}
-
-		return false;
-	}
-
-	private void mark(String state1, String state2) {
-		if (table.containsKey(state1)) {
-			table.get(state1).replace(state2, Status.MARKED);
-		} else if (table.containsKey(state2)) {
-			table.get(state2).replace(state1, Status.MARKED);
-		}
-	}
-
 }
